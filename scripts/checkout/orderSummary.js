d2aeb43a364373886b4a2ec1a7cf65cc2189f3ca
git <babalololuwapelumi9@gmail.com>
2023-12-19 22:39:57 +0100

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import {products, getProduct} from "../../data/products.js";
 import {formatCurrency} from "../utils/money.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from "../../data/deliveryOption.js";
+import {renderPaymentSummary} from "./paymentSummary.js";
 
 export function renderOrderSummary() {
 
@@ -121,6 +122,8 @@ export function renderOrderSummary() {
                 `.js-cart-item-container-${productId}
         `)
             container.remove()
+
+            renderPaymentSummary()
         })
     })
 
@@ -130,6 +133,7 @@ export function renderOrderSummary() {
                     const {productId, deliveryOptionId} = element.dataset
                     updateDeliveryOption(productId, deliveryOptionId)
                     renderOrderSummary()
+                    renderPaymentSummary()
                 })
             }
         )
